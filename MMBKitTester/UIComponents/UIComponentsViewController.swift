fc610db46ec068fc493ae2091c4f0bddbc118a57
Rasid Ramazanov <rasid@Rasid-MacBook-Pro.local>
2020-05-11 15:06:12 +0300

    Latest components.

--- MMBKitTester/UIComponents/UIComponentsViewController.swift
+++ MMBKitTester/UIComponents/UIComponentsViewController.swift
@@ -19,13 +19,13 @@ class UIComponentsViewController: UITableViewController {
         components.modules.append(.init(name: "Key-Value Label", identifier: "KVLabelViewController"))
         components.modules.append(.init(name: "Key-Value List", identifier: "KVListViewController"))
         components.modules.append(.init(name: "Amount-Input View", identifier: "AmountInputViewController"))
-        components.modules.append(.init(name: "Custom Navigation View", identifier: ""))
+//        components.modules.append(.init(name: "Custom Navigation View", identifier: ""))
         components.modules.append(.init(name: "Custom Popups", identifier: "CustomPopupsViewController"))
         components.modules.append(.init(name: "Add-On Features", identifier: "AddOnFeaturesViewController"))
         sections.append(components)
-        sections.append(.init(name: "Custom Dashboard", modules: [
-            .init(name: "Custom Dashboard", identifier: "")
-        ]))
+//        sections.append(.init(name: "Custom Dashboard", modules: [
+//            .init(name: "Custom Dashboard", identifier: "")
+//        ]))
     }
     
 }
