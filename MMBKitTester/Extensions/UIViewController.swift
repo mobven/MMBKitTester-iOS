fc5599534217126455e8dc60e204bfe5d7b165a5
erenbayrakmobven <63582982+erenbayrakmobven@users.noreply.github.com>
2020-06-17 19:00:57 +0300

    custom binder

--- MMBKitTester/Extensions/UIViewController.swift
+++ MMBKitTester/Extensions/UIViewController.swift
@@ -19,3 +19,14 @@ extension UIViewController {
     }
     
 }
+
+extension String {
+    func height(withConstrainedWidth width: CGFloat, font: UIFont) -> CGFloat {
+        let constraintRect = CGSize(width: width, height: .greatestFiniteMagnitude)
+        let boundingBox = self.boundingRect(with: constraintRect,
+                                            options: .usesLineFragmentOrigin,
+                                            attributes: [NSAttributedString.Key.font: font], context: nil)
+        
+        return ceil(boundingBox.height)
+    }
+}
