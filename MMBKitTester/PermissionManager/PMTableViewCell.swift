1c82642939b93ded61bf5776a1ece1ebe5eabded
burakyilmaz50 <burak.yilmaz@mobven.com>
2020-08-21 16:10:23 +0300

    Permission Manager

--- MMBKitTester/PermissionManager/PMTableViewCell.swift
+++ MMBKitTester/PermissionManager/PMTableViewCell.swift
@@ -9,18 +9,289 @@
 import Foundation
 import UIKit
 import PermissionManager
+import NotificationCenter
 
+// swiftlint:disable all
 class PMTableViewCell: UITableViewCell {
-    
     @IBOutlet private weak var permissionName: UILabel!
     @IBOutlet private weak var permissionStatus: UILabel!
+    @IBOutlet weak var grantBttn: UIButton!
     
-    func set() {
+    var permission: Permission!
+    weak var permissionDelagate: PMTableViewCellDelegate!
+    
+    func set(permission: Permission, delagate: PMTableViewCellDelegate) {
+        self.permission = permission
+        self.permissionDelagate = delagate
         
+        checkStatus(permission: permission)
+        
+        permissionName.text = permission.humanReadableString
+    }
+    
+    private func checkStatus(permission: Permission) {
+        switch permission {
+        case .bluetooth:
+            break
+            
+        case .camera:
+            PermissionManager.shared().status { (result: PermissionResult<CameraManager.Result>) in
+                switch result {
+                case .allowed:
+                    self.permissionStatus.text = "Allowed"
+                case .notAllowed:
+                    self.permissionStatus.text = "Not Allowed"
+                case .restricted:
+                    break
+                case .unknown:
+                    break
+                case .notDetermined:
+                    break
+                }
+            }
+        case .photos:
+            PermissionManager.shared().status { (result: PermissionResult<PhotosManager.Result>) in
+                switch result {
+                case .allowed:
+                    self.permissionStatus.text = "Allowed"
+                case .notAllowed:
+                    self.permissionStatus.text = "Not Allowed"
+                case .restricted:
+                    break
+                case .unknown:
+                    break
+                case .notDetermined:
+                    break
+                }
+            }
+        case .notification:
+            PermissionManager.shared().status { (result: PermissionResult<NotificationManager.Result>) in
+                switch result {
+                case .allowed:
+                    DispatchQueue.main.async {
+                        self.permissionStatus.text = "Allowed"
+                    }
+                case .notAllowed:
+                    DispatchQueue.main.async {
+                        self.permissionStatus.text = "Not Allowed"
+                    }
+                case .restricted:
+                    break
+                case .unknown:
+                    break
+                case .notDetermined:
+                    break
+                }
+            }
+        case .calendar:
+            PermissionManager.shared().status { (result: PermissionResult<CalendarManager.Result>) in
+                switch result {
+                case .allowed:
+                    self.permissionStatus.text = "Allowed"
+                case .notAllowed:
+                    self.permissionStatus.text = "Not Allowed"
+                case .restricted:
+                    break
+                case .unknown:
+                    break
+                case .notDetermined:
+                    break
+                }
+            }
+        case .contacts:
+            PermissionManager.shared().status { (result: PermissionResult<ContactsManager.Result>) in
+                switch result {
+                case .allowed:
+                    self.permissionStatus.text = "Allowed"
+                case .notAllowed:
+                    self.permissionStatus.text = "Not Allowed"
+                case .restricted:
+                    break
+                case .unknown:
+                    break
+                case .notDetermined:
+                    break
+                }
+            }
+        case .microphone:
+            PermissionManager.shared().status { (result: PermissionResult<MicrophoneManager.Result>) in
+                switch result {
+                case .allowed:
+                    self.permissionStatus.text = "Allowed"
+                case .notAllowed:
+                    self.permissionStatus.text = "Not Allowed"
+                case .restricted:
+                    break
+                case .unknown:
+                    break
+                case .notDetermined:
+                    break
+                }
+            }
+        case .location(_):
+            PermissionManager.shared().status { (result: PermissionResult<LocationAlwaysManager.Result>) in
+                switch result {
+                case .allowed:
+                    self.permissionStatus.text = "Allowed"
+                case .notAllowed:
+                    self.permissionStatus.text = "Not Allowed"
+                case .restricted:
+                    self.permissionStatus.text = "rRestricted"
+                case .unknown:
+                    break
+                case .notDetermined:
+                    break
+                }
+            }
+        }
     }
     
     @IBAction private func grant() {
-        
+        switch permission! {
+        case .bluetooth:
+            break
+        case .camera:
+            PermissionManager.shared().request { (result : PermissionResult<CameraManager.Result>) in
+                switch result {
+                case .allowed:
+                    print("1")
+                case .notAllowed:
+                    self.permissionDelagate.userDidDenied()
+                case .restricted:
+                    print("4")
+                case .unknown:
+                    print("5")
+                case .notDetermined:
+                    print("5")
+                }
+                self.permissionDelagate.permissionUpdated()
+            }
+        case .photos:
+            PermissionManager.shared().request { (result : PermissionResult<PhotosManager.Result>) in
+                switch result {
+                case .allowed:
+                    print("asd")
+                case .notAllowed:
+                    self.permissionDelagate.userDidDenied()
+                case .restricted:
+                    break
+                case .unknown:
+                    break
+                case .notDetermined:
+                    print("5")
+                }
+                self.permissionDelagate.permissionUpdated()
+            }
+        case .notification:
+            PermissionManager.shared().request { (result : PermissionResult<NotificationManager.Result>) in
+                switch result {
+                case .allowed:
+                    print("asd")
+                case .notAllowed:
+                    self.permissionDelagate.userDidDenied()
+                case .restricted:
+                    break
+                case .unknown:
+                    break
+                case .notDetermined:
+                    print("5")
+                }
+                self.permissionDelagate.permissionUpdated()
+            }
+        case .location:
+            PermissionManager.shared().request { (result : PermissionResult<LocationWhenInUseManager.Result>) in
+                switch result {
+                case .allowed:
+                    print("asd")
+                case .notAllowed:
+                    self.permissionDelagate.userDidDenied()
+                case .restricted:
+                    break
+                case .unknown:
+                    break
+                case .notDetermined:
+                    print("5")
+                }
+                self.permissionDelagate.permissionUpdated()
+            }
+        case .calendar:
+            PermissionManager.shared().request { (result : PermissionResult<LocationAlwaysManager.Result>) in
+                switch result {
+                case .allowed:
+                    print("1")
+                case .notAllowed:
+                    self.permissionDelagate.userDidDenied()
+                case .restricted:
+                    print("4")
+                case .unknown:
+                    print("5")
+                case .notDetermined:
+                    print("5")
+                }
+                self.permissionDelagate.permissionUpdated()
+            }
+        case .contacts:
+            PermissionManager.shared().request { (result : PermissionResult<ContactsManager.Result>) in
+                switch result {
+                case .allowed:
+                    print("1")
+                case .notAllowed:
+                    self.permissionDelagate.userDidDenied()
+                case .restricted:
+                    print("4")
+                case .unknown:
+                    print("5")
+                case .notDetermined:
+                    print("5")
+                }
+                self.permissionDelagate.permissionUpdated()
+            }
+        case .microphone:
+            PermissionManager.shared().request { (result : PermissionResult<MicrophoneManager.Result>) in
+                switch result {
+                case .allowed:
+                    print("1")
+                case .notAllowed:
+                    self.permissionDelagate.userDidDenied()
+                    print("3")
+                case .restricted:
+                    print("4")
+                case .unknown:
+                    print("5")
+                case .notDetermined:
+                    print("5")
+                }
+                self.permissionDelagate.permissionUpdated()
+            }
+        }
     }
-    
+    // swiftlint:enable all
+}
+
+extension Permission {
+    var humanReadableString: String {
+        switch self {
+        case .bluetooth:
+            return "Bluetooth"
+        case .camera:
+            return "camera"
+        case .photos:
+            return "photos"
+        case .notification:
+            return "notification"
+        case .location:
+            return "location"
+        case .calendar:
+            return "calendar"
+        case .contacts:
+            return "contacts"
+        case .microphone:
+            return "microphone"
+        }
+    }
+}
+
+protocol PMTableViewCellDelegate: AnyObject {
+    func permissionUpdated()
+    func userDidDenied()
 }
--- MMBKitTester/PermissionManager/PermissionManagerViewController.swift
+++ MMBKitTester/PermissionManager/PermissionManagerViewController.swift
@@ -12,26 +12,65 @@ import PermissionManager
 
 class PermissionManagerViewController: UITableViewController {
     
+    var permissionTypeArray: [String] = [
+        "bluetooth",
+        "camera",
+        "photos",
+        "notification",
+        "location",
+        "calendar",
+        "contacts",
+        "microphone"
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    
 }
 
 extension PermissionManagerViewController {
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0 // Permission.allCases.count
+        return Permission.allCases.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(
             withIdentifier: "PMTableViewCell", for: indexPath
-            ) as? PMTableViewCell else {
+            ) as? PMTableViewCell
+            
+            else {
                 fatalError("Could not dequeue cell with id")
         }
-        cell.set()
+        cell.set(permission: Permission.allCases[indexPath.row], delagate: self)
         return cell
     }
-    
 }
+    // swiftlint:disable all
+extension PermissionManagerViewController: PMTableViewCellDelegate {
+    func permissionUpdated() {
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
+    }
+    
+    func userDidDenied() {
+        let alert = UIAlertController(title: "Location access required to get your current location",
+                                      message: "Please allow location access", preferredStyle: .alert)
+          let settingsAction = UIAlertAction(title: "Settings", style: .default, handler: { action in
+
+              // open the app permission in Settings app
+              UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!, options: [:], completionHandler: nil)
+          })
+
+          let cancelAction = UIAlertAction(title: "Cancel", style: .default, handler: nil)
+
+          alert.addAction(settingsAction)
+          alert.addAction(cancelAction)
+        
+          alert.preferredAction = settingsAction
+
+          self.present(alert, animated: true, completion: nil)
+    }
+}
+    // swiftlint:enable all
